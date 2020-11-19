5edde2b29d4ee350e5a55f58e3c8b9e046d7bb0e
rimantascode <r.preiks@gmail.com>
2020-11-19 12:30:21 +0000

    fixed allowed hosts was not full url

--- bootique_ado_e/settings.py
+++ bootique_ado_e/settings.py
@@ -23,7 +23,7 @@ SECRET_KEY = 'p@ei#69*b*zz3u4yie-$()@cy^l(+x9&@6ypx+r0lm(3%_9hr7'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['e-commerce-shop-project', 'localhost']
+ALLOWED_HOSTS = ['e-commerce-shop-project.herokuapp.com', 'localhost']
 
 
 # Application definition
