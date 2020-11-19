5c87ec64e6d07c85df6aff54ddc3f9da9c461e31
rimantascode <r.preiks@gmail.com>
2020-11-19 17:47:32 +0000

    enabled automatic deployment

--- custom_storages.py
+++ custom_storages.py
@@ -1,4 +1,4 @@
-
+# custom storages
 from django.conf import settings
 from storages.backends.s3boto3 import S3Boto3Storage
 
@@ -9,3 +9,6 @@ class StaticStorage(S3Boto3Storage):
 
 class MediaStorage(S3Boto3Storage):
     location = settings.MEDIAFILES_LOCATION
+
+
+
