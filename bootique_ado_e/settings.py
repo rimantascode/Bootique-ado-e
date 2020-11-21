3c48eba8204fbc0934222cbf8fdf037e8ba35e30
rimantascode <r.preiks@gmail.com>
2020-11-21 12:32:08 +0000

    added the comment on the cash control

--- bootique_ado_e/settings.py
+++ bootique_ado_e/settings.py
@@ -175,7 +175,7 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 if 'USE_AWS' in os.environ:
-    
+    #Cash control
     AWS_S3_OBJECT_PARAMETERS = {
         'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
         'CacheControl': 'max-age=94608000',
